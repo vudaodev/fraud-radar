c82f435e203db6f2a5a405618e8f8798857c0a1a
vudaodev <v24dao@gmail.com>
2026-08-03 17:17:10 +0100

    Add shared feature transforms for training notebooks and pipeline

---
+++ common/transforms.py
@@ -0,0 +1,8 @@
+'''
+Shared feature transforms. Used by training notebooks and the pickled pipeline.
+'''
+def secs_to_hour_of_day(sec_from_start):
+    ''' Input: seconds since the start of the dataset
+        Output: hour of the day (0-24)
+    '''
+    return (sec_from_start % 86400) / 3600
